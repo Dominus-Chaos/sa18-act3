8f41ffa95b7a4d9ceab85178e9e74b68df94a7bd
Elton Trujillo <eltontrujillo@outlook.com>
2024-05-02 00:11:14 -0500

    added the app installed, fixed views, cant get database or urls working

--- StoreApp/StoreApp/urls.py
+++ StoreApp/StoreApp/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from myapp import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.product_index),
     path('products/', views.product_index, name='product_index'),
-    path('products/<int:id>/', views.product_show, name='product_show'),
+    path('products/<int:id>/', views.product_show, name= 'product_show')
 ]
--- StoreApp/myapp/views.py
+++ StoreApp/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from django.shortcuts import get_object_or_404
 
 def index(request):
     products = Product.objects.all()
@@ -8,3 +9,10 @@ def index(request):
 def show(request, pk):
     product = Product.objects.get(pk=pk)
     return render(request, 'myapp/show.html', {'product': product})
+def product_index(request):
+    products = Product.objects.all()
+    return render(request, 'products/index.html', {'products': products})
+
+def product_show(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'products/show.html', {'product':product})
